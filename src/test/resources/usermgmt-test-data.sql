9869a9627bf8338deafe0bb05c0c913257871bb0
zhangbeiyuan-hw <zhangbeiyuan@huawei.com>
2020-09-10 15:53:22 +0800

    删除测试sql

--- src/test/resources/usermgmt-test-data.sql
+++ src/test/resources/usermgmt-test-data.sql
@@ -10,15 +10,3 @@ VALUES (1, 'APPSTORE', 'GUEST'),
   (7, 'MECM', 'GUEST'),
   (8, 'MECM', 'TENANT'),
   (9, 'MECM', 'ADMIN');
---
---    insert into tbl_role (ID, PLATFORM, ROLE) values(1, 'APPSTORE', 'GUEST');
---    insert into tbl_role (ID, PLATFORM, ROLE) values(2, 'APPSTORE', 'TENANT');
---    insert into tbl_role (ID, PLATFORM, ROLE) values(3, 'APPSTORE', 'ADMIN');
---
---    insert into tbl_role (ID, PLATFORM, ROLE) values(4, 'DEVELOPER', 'GUEST');
---    insert into tbl_role (ID, PLATFORM, ROLE) values(5, 'DEVELOPER', 'TENANT');
---    insert into tbl_role (ID, PLATFORM, ROLE) values(6, 'DEVELOPER', 'ADMIN');
---
---    insert into tbl_role (ID, PLATFORM, ROLE) values(7, 'MECM', 'GUEST');
---    insert into tbl_role (ID, PLATFORM, ROLE) values(8, 'MECM', 'TENANT');
---    insert into tbl_role (ID, PLATFORM, ROLE) values(9, 'MECM', 'ADMIN');
