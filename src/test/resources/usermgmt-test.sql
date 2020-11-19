ca171a22faecf225fca23273a8eebe64c26b83d2
zhangbeiyuan-hw <zhangbeiyuan@huawei.com>
2020-11-19 14:50:30 +0800

    调试测试用例

--- src/test/resources/usermgmt-test.sql
+++ src/test/resources/usermgmt-test.sql
@@ -6,14 +6,16 @@ CREATE TABLE if not exists tbl_tenant (
   	COMPANY             VARCHAR(50)       NULL,
   	TELEPHONENUMBER     VARCHAR(20)       NOT NULL,
   	GENDER              VARCHAR(10)       NULL,
+  	isallowed           boolean           NOT NULL,
   	CONSTRAINT USERNAME UNIQUE (username),
   	CONSTRAINT TELEPHONENUMBER UNIQUE (telephonenumber)
 
 );
 
 CREATE TABLE if not exists tbl_tenant_role (
- 	  TENANTID            VARCHAR(100)      NOT NULL,
-  	ROLEID              INTEGER           NOT NULL
+ 	TENANTID            VARCHAR(100)      NOT NULL,
+  	ROLEID              INTEGER           NOT NULL,
+    primary key(TENANTID, ROLEID)
 );
 
 
