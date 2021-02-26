42314e1103d2b3d226da0f558b74ff4067c3625d
zhouyanbing <zhouyanbing.zhou@huawei.com>
2021-02-26 10:24:28 +0800

    通过邮箱获取验证码的接口权限放开

--- src/main/java/org/edgegallery/user/auth/config/security/SecurityConfig.java
+++ src/main/java/org/edgegallery/user/auth/config/security/SecurityConfig.java
@@ -69,7 +69,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 "/js/**", "/favicon.ico",
                 "/index.html", "/user-privacy.md", "/user-agreement.md")
             .permitAll()
-            .antMatchers(HttpMethod.POST, "/v1/users", "/v1/users/action/uniqueness", "/v1/identity/sms")
+            .antMatchers(HttpMethod.POST, "/v1/users", "/v1/users/action/uniqueness", "/v1/identity/sms", "/v1/identity/mail")
             .permitAll()
             .antMatchers(HttpMethod.PUT, "/v1/users/password")
             .permitAll()
