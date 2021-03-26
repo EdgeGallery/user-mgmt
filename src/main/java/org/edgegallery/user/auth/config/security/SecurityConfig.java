eeb5228920d4b77824cbca9640a60935edd8e707
zhangbeiyuan-hw <zhangbeiyuan@huawei.com>
2021-03-26 11:45:53 +0800

    修改静态告警 ，增加测试用例

--- src/main/java/org/edgegallery/user/auth/config/security/SecurityConfig.java
+++ src/main/java/org/edgegallery/user/auth/config/security/SecurityConfig.java
@@ -31,7 +31,6 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.cors.CorsConfiguration;
