d3ea8e2bc93a87c4a7549b8a27729f34c2ca7fe4
zhouyanbing <zhouyanbing.zhou@huawei.com>
2021-12-17 15:11:48 +0800

    UT用例实现方式修改

--- src/test/java/org/edgegallery/user/auth/service/VerificationTest.java
+++ src/test/java/org/edgegallery/user/auth/service/VerificationTest.java
@@ -115,7 +115,15 @@ public class VerificationTest {
     public void mail_should_successfully_when_disabled() {
         VerificationReqByMailDto request = new VerificationReqByMailDto();
         request.setMailAddress("13800000003@edgegallery.org");
+        MockUp booleanMockup = new MockUp<Boolean>() {
+            @Mock
+            public boolean parseBoolean(String boolValue) {
+                return false;
+            }
+        };
         Either<Boolean, FormatRespDto> either = identityService.sendVerificationCodeByMail(request);
+        booleanMockup.tearDown();
+
         Assert.assertTrue(either.isLeft());
         Assert.assertTrue(either.left().value());
     }
@@ -130,13 +138,15 @@ public class VerificationTest {
                 return true;
             }
         };
-        new MockUp<MailService>() {
+        MockUp mailServiceMockup = new MockUp<MailService>() {
             @Mock
             public boolean sendSimpleMail(String receiver, String subject, String content) {
                 return true;
             }
         };
         Either<Boolean, FormatRespDto> either = identityService.sendVerificationCodeByMail(request);
+        mailServiceMockup.tearDown();
+
         Assert.assertTrue(either.isLeft());
         Assert.assertTrue(either.left().value());
     }
@@ -166,12 +176,6 @@ public class VerificationTest {
                 return true;
             }
         };
-        new MockUp<MailService>() {
-            @Mock
-            public boolean sendSimpleMail(String receiver, String subject, String content) {
-                return false;
-            }
-        }.tearDown();
         Either<Boolean, FormatRespDto> either = identityService.sendVerificationCodeByMail(request);
         Assert.assertTrue(either.isRight());
         Assert.assertEquals(either.right().value().getErrStatus(), Response.Status.EXPECTATION_FAILED);
